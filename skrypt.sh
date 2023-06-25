04855cc937cfe0021489ff26629484c5d92fc6b8
Natalia Zygmunt <nataliazygmuntfon@gmail.com>
2023-06-25 20:44:21 +0200

    Extended script.sh with --error|-e option

--- skrypt.sh
+++ skrypt.sh
@@ -4,13 +4,17 @@ while test $# -gt 0; do
   case "$1" in
     -h|--help)
 	  echo "Sposoby użycia skryptu:"
-      echo "skrypt.sh --date   -   wyswietla dzisiejsza date"
-      echo "skrypt.sh --logs   -   tworzy 100 plików o nazwach logx.txt, gdzie x to numer pliku"
-	  echo "                       w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
-	  echo "skrypt.sh --logs N -   tworzy N plików o nazwach logx.txt, gdzie x to numer pliku"
-	  echo "                       w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
-	  echo "skrypt.sh --init   -   klonuje całe repozytorium do katalogu w którym został uruchomiony oraz ustawia ścieżkę w zmiennej środowiskowej PATH"
-	  echo "skrypt.sh --help   -   pokazuje te informacje"
+      echo "skrypt.sh --date    -   wyswietla dzisiejsza date"
+      echo "skrypt.sh --logs    -   tworzy 100 plików o nazwach logx.txt, gdzie x to numer pliku"
+	  echo "                        w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
+	  echo "skrypt.sh --logs N  -   tworzy N plików o nazwach logx.txt, gdzie x to numer pliku"
+	  echo "                        w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
+      echo "skrypt.sh --error   -   tworzy 100 plików o nazwach errorx.txt, gdzie x to numer pliku"
+	  echo "                        w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
+	  echo "skrypt.sh --error N -   tworzy N plików o nazwach errorx.txt, gdzie x to numer pliku"
+	  echo "                        w każdym pliku jest jego nazwa, nazwa skryptu który go utworzył i data"
+	  echo "skrypt.sh --init    -   klonuje całe repozytorium do katalogu w którym został uruchomiony oraz ustawia ścieżkę w zmiennej środowiskowej PATH"
+	  echo "skrypt.sh --help    -   pokazuje te informacje"
       exit 0
       ;;
     -d|--date)
@@ -35,6 +39,20 @@ while test $# -gt 0; do
 	    echo log$i.txt > log$i.txt;
 	    echo `basename $0` >> log$i.txt;
 	    echo $DATE >> log$i.txt;
+	  done
+      ;;
+    -e|--error)
+	  shift
+	  MAX=100
+      if test $# -gt 0; then
+        MAX=$1
+      fi
+	  shift
+      DATE=`date`
+	  for ((i = 1 ; i <= $MAX ; i++ )); do
+	    echo error$i.txt > error$i.txt;
+	    echo `basename $0` >> error$i.txt;
+	    echo $DATE >> error$i.txt;
 	  done
       ;;
     *)
